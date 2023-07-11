ab80ef00cf6c734ca256e56474a2d317a2c49ca2
Albin Robin <quic_albirobi@quicinc.com>
2023-07-11 13:16:06 -0400

    virtio-video: parse qmenu passed from BE
    Qmenu passed from BE is restructured into a null-terminated array of
    string pointers. This array is used for private qmenu controls.
    
    Change-Id: If6bc7d29107ee0f6e89224fd664f9aeeb6f823e0

--- driver/virtio_video_dec.c
+++ driver/virtio_video_dec.c
@@ -111,25 +111,6 @@ static const struct v4l2_ctrl_ops virtio_video_dec_ctrl_ops = {
 #endif
 };
 
-
-static const char * const hevc_profile[] = {
-	"Main",
-	"Main Still Picture",
-	"Main 10",
-	NULL,
-};
-
-static const char * const * msm_vidc_get_qmenu_type(u32 control_id)
-{
-	switch (control_id) {
-		case V4L2_CID_MPEG_VIDEO_HEVC_PROFILE:
-			return hevc_profile;
-		default:
-			return NULL;
-	}
-}
-
-
 int virtio_video_dec_init_ctrls(struct virtio_video_stream *stream)
 {
 #ifdef VIRTIO_VIDEO_MSM
@@ -167,7 +148,9 @@ int virtio_video_dec_init_ctrls(struct virtio_video_stream *stream)
 
 			if (ctrl_cfg.type == V4L2_CTRL_TYPE_MENU) {
 				ctrl_cfg.menu_skip_mask = ~(config->step);
-				ctrl_cfg.qmenu = msm_vidc_get_qmenu_type(config->id);
+				ctrl_cfg.qmenu = (const char * const *)(
+						 (char*)config +
+						 config->qmenu_offset);
 				ctrl_cfg.step = 0;
 			} else {
 				ctrl_cfg.step = config->step;
--- driver/virtio_video_device.c
+++ driver/virtio_video_device.c
@@ -1182,6 +1182,31 @@ static void virtio_video_device_unregister(struct virtio_video_device *vvd)
 }
 
 #ifdef VIRTIO_VIDEO_MSM
+
+#define MAX_QMENU_ELEMENT_COUNT     64
+/* When passing qmenu from BE, it is structured with qmenu length, followed by
+ * data offsets, followed by actual qmenu data. This function restructures the
+ * same buffer into a NULL-terminated array of string pointers.
+ */
+static void virtio_video_parse_qmenu(char *menu_buf, int size)
+{
+	int64_t element_count = *(int64_t *)menu_buf;
+	int64_t *offsets = (int64_t *)(menu_buf + sizeof(int64_t));
+	char **qmenu_list = (char **)menu_buf;
+	int menu_iter = 0;
+
+	if ((element_count <= MAX_QMENU_ELEMENT_COUNT) &&
+	    ((element_count + 1) * sizeof(int64_t) < size)) {
+		for (menu_iter = 0; menu_iter < element_count; menu_iter++) {
+			if (offsets[menu_iter] < size)
+				qmenu_list[menu_iter] = menu_buf +
+							offsets[menu_iter];
+		}
+	}
+
+	qmenu_list[menu_iter] = NULL;
+}
+
 static int virtio_video_parse_controls(struct virtio_video_device *vvd,
 				       void *resp_buf, bool *is_end)
 {
@@ -1219,6 +1244,13 @@ static int virtio_video_parse_controls(struct virtio_video_device *vvd,
 			ret = -ENOMEM;
 			goto err_ctrl;
 		}
+
+		/* parse qmenu - replace count/offsets with pointers. */
+		if (new_config->type == V4L2_CTRL_TYPE_MENU)
+			virtio_video_parse_qmenu((char*)new_config +
+						 new_config->qmenu_offset,
+						 new_config->size -
+						 new_config->qmenu_offset);
 
 		ctrl->config = new_config;
 		list_add_tail(&ctrl->ctrls_list_entry, &vvd->ctrl_config_list);
