88cad4927ce4cfb5f000025204ca4f2fec7c81bd
Robert Qiu <quic_rqiu@quicinc.com>
2025-05-08 23:03:09 -0400

    virtio-video: Enable virtio video for gen5 platform
    Both gen5's HQX and HGY platform support virtio-video.
    
    Change-Id: Ib84000b672e9a014fe590e12a3ce64ac6d682412

--- virtio_video_kernel_board.mk
+++ virtio_video_kernel_board.mk
@@ -1,27 +1,35 @@
 # SPDX-License-Identifier: GPL-2.0-only
+
 TARGET_VIRTIO_VIDEO_ENABLE := false
-ifeq ($(TARGET_USES_GY), true)
-    ifeq ($(TARGET_KERNEL_DLKM_DISABLE), true)
-        ifeq ($(TARGET_KERNEL_DLKM_VIDEO_OVERRIDE), true)
-        TARGET_VIRTIO_VIDEO_ENABLE := true
-        endif
-    else
+ifeq ($(ENABLE_HYP),true)
+  ifeq ($(TARGET_BOARD_PLATFORM),gen5)
     TARGET_VIRTIO_VIDEO_ENABLE := true
+  else ifeq ($(TARGET_USES_GY), true)
+    TARGET_VIRTIO_VIDEO_ENABLE := true
+  endif
+endif
+
+#check DLKM and OVERRIDE flags
+ifeq ($(TARGET_VIRTIO_VIDEO_ENABLE),true)
+  ifeq ($(TARGET_KERNEL_DLKM_DISABLE),true)
+    ifeq ($(TARGET_KERNEL_DLKM_VIDEO_OVERRIDE),false)
+      TARGET_VIRTIO_VIDEO_ENABLE := false
+      $(warning "Virtio-video disabled due to DLKM overide")
     endif
+  endif
 endif
 
 # Check QMAA flags
-# Temporary disable checking until target side enable override
-#ifeq ($(TARGET_USES_QMAA), true)
-#    ifneq ($(TARGET_USES_QMAA_OVERRIDE_VIDEO), true)
-#    TARGET_VIRTIO_VIDEO_ENABLE := false
-#    $(warning "virtio-video disabled due to QMAA")
-#    endif
-#endif
+ifeq ($(TARGET_USES_QMAA), true)
+  ifneq ($(TARGET_USES_QMAA_OVERRIDE_VIDEO), true)
+    TARGET_VIRTIO_VIDEO_ENABLE := false
+    $(warning "Virtio-video disabled due to QMAA")
+  endif
+endif
 
 # Build video kernel driver
 ifeq ($(TARGET_VIRTIO_VIDEO_ENABLE),true)
-BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm_virtio_video.ko
-
-BUILD_VIDEO_TECHPACK_SOURCE := true
+  $(warning "Virtio-video board vendor enabled")
+  BOARD_VENDOR_KERNEL_MODULES += $(KERNEL_MODULES_OUT)/msm_virtio_video.ko
+  BUILD_VIDEO_TECHPACK_SOURCE := true
 endif
--- virtio_video_kernel_product.mk
+++ virtio_video_kernel_product.mk
@@ -1,24 +1,34 @@
 # SPDX-License-Identifier: GPL-2.0-only
+
 TARGET_VIRTIO_VIDEO_ENABLE := false
-ifeq ($(TARGET_USES_GY), true)
-    ifeq ($(TARGET_KERNEL_DLKM_DISABLE), true)
-        ifeq ($(TARGET_KERNEL_DLKM_VIDEO_OVERRIDE), true)
-        TARGET_VIRTIO_VIDEO_ENABLE := true
-        endif
-    else
+ifeq ($(ENABLE_HYP),true)
+  ifeq ($(TARGET_BOARD_PLATFORM),gen5)
     TARGET_VIRTIO_VIDEO_ENABLE := true
+  else ifeq ($(TARGET_USES_GY), true)
+    TARGET_VIRTIO_VIDEO_ENABLE := true
+  endif
+endif
+
+#check DLKM and OVERRIDE flags
+ifeq ($(TARGET_VIRTIO_VIDEO_ENABLE),true)
+  ifeq ($(TARGET_KERNEL_DLKM_DISABLE),true)
+    ifeq ($(TARGET_KERNEL_DLKM_VIDEO_OVERRIDE),false)
+      TARGET_VIRTIO_VIDEO_ENABLE := false
+      $(warning "virtio-video disabled due to DLKM overide")
     endif
+  endif
 endif
 
 # Check QMAA flags
-# Temporary disable checking until target side enable override
-#ifeq ($(TARGET_USES_QMAA), true)
-#    ifneq ($(TARGET_USES_QMAA_OVERRIDE_VIDEO), true)
-#    TARGET_VIRTIO_VIDEO_ENABLE := false
-#    $(warning "virtio-video disabled due to QMAA")
-#    endif
-#endif
+ifeq ($(TARGET_USES_QMAA), true)
+  ifneq ($(TARGET_USES_QMAA_OVERRIDE_VIDEO), true)
+    TARGET_VIRTIO_VIDEO_ENABLE := false
+    $(warning "virtio-video disabled due to QMAA")
+  endif
+endif
 
+# Build video kernel driver
 ifeq ($(TARGET_VIRTIO_VIDEO_ENABLE),true)
-PRODUCT_PACKAGES  += $(KERNEL_MODULES_OUT)/msm_virtio_video.ko
+  $(warning Virtio-video product package enabled")
+  PRODUCT_PACKAGES  += $(KERNEL_MODULES_OUT)/msm_virtio_video.ko
 endif
