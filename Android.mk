d70674276a420987e4c06e83f54c224c11dfc11c
Vishak V S <vsakunth@qti.qualcomm.com>
2026-05-14 12:40:51 +0530

    virtio-video: Add Android.mk to compile gen4 or gen5 target
    Added Android.mk in root folder that builds gen4 or
    gen5 based on the compilation target, we are using
    TARGET_BOARD_PLATFORM to determine if gen4 or gen5 is
    being compiled. Without this Android.mk in both gen4
    and gen5 will be included as there is no Android.mk
    in parent folder.
    
    Change-Id: I109f218f01f5973a86386a645be630d04a8bd325
    Signed-off-by: Vishak V S <vsakunth@qti.qualcomm.com>

--- Android.mk
+++ Android.mk
@@ -1,3 +1,6 @@
-ifneq ($(filter gen5,$(TARGET_BOARD_PLATFORM)),)
+# Select gen4/gen5 build based on the target platform
+ifneq ($(filter gen4,$(TARGET_BOARD_PLATFORM)),)
+include $(call my-dir)/gen4/Android.mk
+else ifneq ($(filter gen5,$(TARGET_BOARD_PLATFORM)),)
 include $(call my-dir)/gen5/Android.mk
 endif
